177b55e559219a09516b1c7c2f899f927c85ec47
eejones <joneserice@gmail.com>
2014-05-11 15:22:18 -0500

    Now displaying distinct values for filtering in index by attribute as well as keeping track of attributes correctly

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,4 +36,3 @@ $(function() {
     return false;
   });
 });
-
--- app/controllers/dynamic_select/manufacturers_controller.rb
+++ app/controllers/dynamic_select/manufacturers_controller.rb
@@ -8,6 +8,10 @@ module DynamicSelect
       @manufacturers = Manufacturer.joins("LEFT JOIN `modtypemanufacturers` ON `modtypemanufacturers`.`manufacturer_id` = `manufacturers`.`id`").where(:modtypemanufacturers => {:modtype_id => params[:modtype_id]})
       respond_with(@manufacturers)
     end
+
+    def allfilter
+      @manufacturers = @products.where((:modality_id||nil) == 'modality_id').where(:modtype_id||nil) == 'modtype_id')
+    end
   end
 end
 
